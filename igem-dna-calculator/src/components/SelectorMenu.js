54643d6ac8ff0d6e9eead22ba43dce1e6b24e095
SatanshuMishra <satanshumishra@outlook.com>
2023-07-05 22:33:39 -0700

    V1.2 Fixed bug where SelectorMenu wouldn't close when clicking outside menu.

--- igem-dna-calculator/src/components/SelectorMenu.js
+++ igem-dna-calculator/src/components/SelectorMenu.js
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 
 function Selector(props) {
   const dataset = props.dataset;
   const [isActive, setIsActive] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
+
   const toggleMenu = () => {
     setIsActive(!isActive);
   };
+
+  const menuRef = useRef(null);
+
+  const closeMenu = (e) => {
+    if (menuRef.current && isActive && !menuRef.current.contains(e.target)) {
+      setIsActive(false);
+    }
+  };
+
+  document.addEventListener("mousedown", closeMenu);
+
   const setSelection = (selection) => {
     setSelectedOption(selection);
     toggleMenu();
     props.selectedValue(selection);
   };
+
   return (
     <div className="flex-1">
       <div
@@ -22,7 +35,10 @@ function Selector(props) {
         {selectedOption}
       </div>
       {isActive && (
-        <div className="m-2 text-black w-60 bg-white text-base rounded-md text-left mt-1 shadow-lg cursor-pointer absolute">
+        <div
+          className="m-2 text-black w-60 bg-white text-base rounded-md text-left mt-1 shadow-lg cursor-pointer absolute"
+          ref={menuRef}
+        >
           {dataset.map((data) => {
             let value = data.value;
             return (
